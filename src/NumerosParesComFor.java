35f34876f6801e375b6d6148006f3831bd4cb2f7
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-08 07:41:50 -0300

    Numeros Pares com For

---
+++ src/NumerosParesComFor.java
@@ -0,0 +1,11 @@
+public class NumerosParesComFor {
+    public static void main(String[] args) {
+        //Imprima os numeros pares de 0 até 100
+
+        for (int i=0; i < 100; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
