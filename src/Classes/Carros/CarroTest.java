ac4b98467d527cd89318c1ac1a86d4e5ac098c7f
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-16 20:30:07 -0300

    mudanca no exercicio de criar classe carro

--- src/Classes/Carros/CarroTest.java
+++ src/Classes/Carros/CarroTest.java
@@ -1,11 +1,18 @@
 package Classes.Carros;
 
+
+import java.util.Scanner;
+
 public class CarroTest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Carros carro1 = new Carros();
-        carro1.ano = 1972;
-        carro1.nome = "uno";
-        carro1.modelo = "SPORTIVO";
+        System.out.println("Digite o nome do carro: ");
+        carro1.nome = scanner.next();
+        System.out.println("Digite o ano do carro: ");
+        carro1.ano = scanner.nextInt();
+        System.out.println("Digite o modelo do carro: ");
+        carro1.modelo = scanner.next();
 
         System.out.println(carro1.nome + " " + carro1.modelo + " " + carro1.ano);
     }
