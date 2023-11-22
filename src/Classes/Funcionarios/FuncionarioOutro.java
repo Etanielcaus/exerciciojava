1c41223dd8a36c9a39e352319f8a68d5879b0eda
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-22 07:02:56 -0300

    get e set pt02

--- src/Classes/Funcionarios/FuncionarioOutro.java
+++ src/Classes/Funcionarios/FuncionarioOutro.java
@@ -1,9 +1,38 @@
 package Classes.Funcionarios;
 
 public class FuncionarioOutro {
-    public String nome;
-    public int idade;
-    public double[] salario;
+    private String nome;
+    private int idade;
+    private double[] salario;
+    private double media = 0;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double[] salario) {
+        this.salario = salario;
+    }
+
+    public double getMedia() {
+        return media;
+    }
 
     public void imprimir(){
         System.out.println(this.nome);
@@ -21,7 +50,7 @@ public class FuncionarioOutro {
     }
 
     private void media(){
-        double media = 0;
+
         for (double salario : this.salario) {
             media += salario;
         }
--- src/Classes/Funcionarios/FuncionarioOutroInst.java
+++ src/Classes/Funcionarios/FuncionarioOutroInst.java
@@ -3,11 +3,11 @@ package Classes.Funcionarios;
 public class FuncionarioOutroInst {
     public static void main(String[] args) {
         FuncionarioOutro funcionario01 = new FuncionarioOutro();
-        funcionario01.nome = "João";
-        funcionario01.idade = 20;
+        funcionario01.setNome("Naruto");
+        funcionario01.setIdade(20);
 //        funcionario01.salario = null;
-        //funcionario01.salario = new double[]{1560, 1720, 1200};
-
+        funcionario01.setSalario(new double[]{1560, 1720, 1200});
+        System.out.println(funcionario01.getSalario());
         funcionario01.imprimir();
 
     }
