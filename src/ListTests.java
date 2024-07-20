ad3ef5e69048dec59917e6f6b2335e9e14afba3c
Etanielcaus <nielfilosofia@gmail.com>
2024-07-19 22:04:52 -0300

    test Reduce Streams

--- src/ListTests.java
+++ src/ListTests.java
@@ -20,9 +20,17 @@ public class ListTests {
 
         Alunos aluno1 = new Alunos("lula", 75);
         Alunos aluno2 = new Alunos("Bolsonaro", 65);
+        Alunos joao = new Alunos("Joao", 18);
+        Alunos pedro = new Alunos("Pedro", 75);
+        Alunos andrei = new Alunos("andrei", 21);
+        Alunos sergio = new Alunos("Sergio", 15);
+        Alunos maria = new Alunos("Maria", 13);
+        Alunos ana = new Alunos("Ana", 11);
+        Alunos jose = new Alunos("Jose", 18);
 
         lista1.add(aluno1);
         lista1.add(aluno2);
+
 
         imprimeLista(lista1);
 
@@ -33,10 +41,28 @@ public class ListTests {
 
         lista2.add(aluno1);
         lista2.add(aluno2);
+        lista2.add(joao);
+        lista2.add(pedro);
+        lista2.add(andrei);
+        lista2.add(sergio);
+        lista2.add(maria);
+        lista2.add(ana);
+        lista2.add(jose);
 
 //        lista2.forEach(System.out::println);
 
-        List<Alunos> collect = lista2.stream().filter(aluno -> aluno.getIdade() <= 65).toList();
-        collect.forEach(System.out::println);
+//        List<Alunos> collect = lista2.stream().filter(aluno -> aluno.getIdade() <= 65).toList();
+//        collect.forEach(System.out::println);
+
+//        Pega todos os que possuem idade menor ou igual a 18
+        lista2.stream().map(Alunos::getIdade)
+                .filter(a -> a <= 18)
+                .forEach(System.out::println);
+
+        System.out.println("Soma das idades...");
+        lista2.stream().map(Alunos::getIdade)
+                .filter(a -> a <= 18)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
     }
 }
