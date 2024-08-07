e9e3cf2e33d509ff74e1c47adcfeb6b424bd6e81
Etanielcaus <nielfilosofia@gmail.com>
2024-08-06 21:02:39 -0300

    adding the lombok in the project

--- src/main/java/JDBCtest/Dominio/TestNames.java
+++ src/main/java/JDBCtest/Dominio/TestNames.java
@@ -1,59 +1,64 @@
 package JDBCtest.Dominio;
 
+import lombok.Builder;
+import lombok.Value;
+
 import java.util.Objects;
 
+@Value
+@Builder
 public class TestNames {
-    private Integer id;
-    private String name;
+    Integer id;
+    String name;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TestNames testNames = (TestNames) o;
-        return Objects.equals(id, testNames.id) && Objects.equals(name, testNames.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-
-    public static final class TestNamesBuilder {
-        private Integer id;
-        private String name;
-
-        private TestNamesBuilder() {
-        }
-
-        public static TestNamesBuilder builder() {
-            return new TestNamesBuilder();
-        }
-
-        public TestNamesBuilder withId(Integer id) {
-            this.id = id;
-            return this;
-        }
-
-        public TestNamesBuilder withName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public TestNames build() {
-            TestNames testNames = new TestNames();
-            testNames.id = this.id;
-            testNames.name = this.name;
-            return testNames;
-        }
-    }
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        TestNames testNames = (TestNames) o;
+//        return Objects.equals(id, testNames.id) && Objects.equals(name, testNames.name);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, name);
+//    }
+//
+//
+//    public static final class TestNamesBuilder {
+//        private Integer id;
+//        private String name;
+//
+//        private TestNamesBuilder() {
+//        }
+//
+//        public static TestNamesBuilder builder() {
+//            return new TestNamesBuilder();
+//        }
+//
+//        public TestNamesBuilder withId(Integer id) {
+//            this.id = id;
+//            return this;
+//        }
+//
+//        public TestNamesBuilder withName(String name) {
+//            this.name = name;
+//            return this;
+//        }
+//
+//        public TestNames build() {
+//            TestNames testNames = new TestNames();
+//            testNames.id = this.id;
+//            testNames.name = this.name;
+//            return testNames;
+//        }
+//    }
 }
--- src/main/java/JDBCtest/Test/ConnectionTest.java
+++ src/main/java/JDBCtest/Test/ConnectionTest.java
@@ -1,13 +1,11 @@
 package JDBCtest.Test;
 
-import JDBCtest.Connection.ConnectionFactory;
 import JDBCtest.Dominio.TestNames;
 import JDBCtest.Repository.NameTestRepository;
 
 public class ConnectionTest {
     public static void main(String[] args) {
-        TestNames joaoCarlos = TestNames.TestNamesBuilder.builder()
-                .withName("Pedro Vinicius").build();
+        TestNames joaoCarlos = TestNames.builder().name("Pedrão").build();
 
         NameTestRepository.save(joaoCarlos);
     }
